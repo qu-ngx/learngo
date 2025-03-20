9df3434697b9289b363e9fe8358a918981c74585
Quang Nguyễn <quangnguyen@Quangs-MacBook-Air-7.local>
2025-03-20 18:03:01 -0400

    added go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var intNum int = 32767
+	fmt.Print(intNum)
+}
+
+func printMe(printValue string) {
+	var QuangNguyen string = "QuangKoNgu"
+	fmt.Println(QuangNguyen)
+}
